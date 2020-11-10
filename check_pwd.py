679fa558f062063dda8290634ef0877fa66a1d0d
Daniel Kim <kim.danieljw@gmail.com>
2020-11-10 04:15:56 -0500

    5th commit updated pwd_check to check for presence of at least 1 uppercase letter

--- check_pwd.py
+++ check_pwd.py
@@ -4,13 +4,16 @@ import string
 def check_pwd(pwd):
     # check for length greater than or equal 8, less than or eq to 20
     pwd_length = len(pwd)
-    lower_case = string.ascii_lowercase
     pwd_status = True
     if 8 <= pwd_length <= 20:
         pwd_status = True
     else:
         return False
     if any(char.islower() for char in pwd):
+        pwd_status = True
+    else:
+        return False
+    if any(char.isupper() for char in pwd):
         pwd_status = True
     else:
         return False
