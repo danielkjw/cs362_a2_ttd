2feceec51b13135d83986fe3aea0d1d9b1462483
Daniel Kim <kim.danieljw@gmail.com>
2020-11-10 02:21:04 -0500

    1st commit test1fail- begin coding_edited initial status_redit

--- check_pwd.py
+++ check_pwd.py
@@ -1,5 +1,4 @@
 
 def check_pwd(pwd):
-    # test should return non boolean
-    pwd_status = 1
-    return pwd_status
+    return True
+
--- tests.py
+++ tests.py
@@ -4,8 +4,8 @@ from check_pwd import check_pwd
 
 class TestCase(unittest.TestCase):
     def test1_init_test(self):
-        input = 1
-        expected = True
+        input = '1'
+        expected = False
         self.assertFalse(check_pwd(input), expected)
 
 
