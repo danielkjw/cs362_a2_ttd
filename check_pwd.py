6bf19b7e84cda135ddb0aab85409d20986aea96e
Daniel Kim <kim.danieljw@gmail.com>
2020-11-10 03:58:50 -0500

    4th commit updated to check pwd to be >= 8 and <= 20, was incorrect before by mistake(uninentional)

--- check_pwd.py
+++ check_pwd.py
@@ -2,7 +2,7 @@
 def check_pwd(pwd):
     # check for length greater than or equal 8, less than or eq to 20
     pwd_length = len(pwd)
-    if pwd_length <= 8 and pwd_length <= 20:
+    if 8 <= pwd_length <= 20:
         pwd_status = True
     else:
         pwd_status = False
