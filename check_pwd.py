6e89b959146cbdb15b6e5177d80662af855fc807
Daniel Kim <kim.danieljw@gmail.com>
2020-11-10 02:35:39 -0500

    made check_pwd return false if length is less than 8

--- check_pwd.py
+++ check_pwd.py
@@ -2,10 +2,10 @@
 def check_pwd(pwd):
     # check for length greater than or equal 8, less than or eq to 20
     pwd_length = len(pwd)
-    if pwd_length != 0:
+    if pwd_length >= 8:
         pwd_status = True
     else:
         pwd_status = False
-    return True
+    return pwd_status
 
 
