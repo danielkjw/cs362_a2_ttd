b6586cf74a03d67addb3aa84b4c94397e783577a
Daniel Kim <kim.danieljw@gmail.com>
2020-11-10 04:11:38 -0500

    5th commit updated check_pwd to check if pwd has at least one lowercase

--- check_pwd.py
+++ check_pwd.py
@@ -1,11 +1,20 @@
+import string
+
 
 def check_pwd(pwd):
     # check for length greater than or equal 8, less than or eq to 20
     pwd_length = len(pwd)
+    lower_case = string.ascii_lowercase
+    pwd_status = True
     if 8 <= pwd_length <= 20:
         pwd_status = True
     else:
-        pwd_status = False
+        return False
+    if any(char.islower() for char in pwd):
+        pwd_status = True
+    else:
+        return False
+
     return pwd_status
 
 
